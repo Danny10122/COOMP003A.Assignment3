9cb3a38df9b7989ff1d84011de7a2e2c463a6fdd
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-31 00:27:13 -0800

    Just set everything up to put my inputs to make it work

--- COOMP003A.Assignment3/Program.cs
+++ COOMP003A.Assignment3/Program.cs
@@ -6,7 +6,30 @@
         {
             Console.WriteLine("=== Academic & Personal Readiness Evalutor ===");
 
-            Console.WriteLine
+            Console.Write("\nCurrent Year: ");
+
+            Console.Write("Enter your birth year: ");
+
+            Console.Write("\nCalculated Age: ");
+
+            Console.Write("Readiness Category");
+
+            Console.Write("\nDo you have a valid ID? (yes/no): ");
+
+            Console.Write("\nReadiness Status: ");
+
+            Console.WriteLine("\nSelect a guidance option:");
+
+            Console.WriteLine("1 - Academic Planning");
+
+            Console.WriteLine("2 - Personal Planning");
+
+            Console.WriteLine("3 - Career Planning");
+
+            Console.Write("Enter choice: ");
+
+            Console.WriteLine("\nGuidance:");
+            Console.WriteLine("Meet with an academic advisor to plan your next term.");
         }
     }
 }
